0bd0d6517d13bc7745bb9cf7ff244b096d27475c
facundoHarche1 <facundoharche1@gmail.com>
2026-08-12 00:13:44 -0300

    Agrego mensajes de error

--- src/main/java/ar/edu/utn/dds/k3003/controllers/GlobalExceptionHandler.java
+++ src/main/java/ar/edu/utn/dds/k3003/controllers/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ar.edu.utn.dds.k3003.controllers;
 import ar.edu.utn.dds.k3003.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -32,5 +33,16 @@ public class GlobalExceptionHandler {
             DepositoLleno.class})
     public ResponseEntity<String> handleBadRequest(Exception e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<String> handleNumberFormat(NumberFormatException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El ID debe ser un valor numérico válido");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleMalformedBody(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("El cuerpo de la solicitud es inválido o contiene un valor no soportado");
     }
 }
